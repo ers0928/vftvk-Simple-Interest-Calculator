c196b8a2ad46b849da7d92abbf0b6bd2b5f0c36d
Elissa Sheridan <ers0928@gmail.com>
2021-09-26 23:02:20 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -6,10 +6,8 @@ function compute()
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest =principal*years*rate/100;
-    //defining amount as principal plus interest for span results
-    var amount = parseInt(principal)+ parseInt(interest);
     var year = new Date().getFullYear()+parseInt(years);
-      document.getElementById("result").innerHTML= "If you deposit <mark>$"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>$"+amount+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"; 
+      document.getElementById("result").innerHTML= "If you deposit <mark>$"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>$"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"; 
 }
 //add rate display
 function updateRate()
